425882a009cbd0cbaa67fde63ba8f24be8c50a97
Gabriela Barrera <Gabrielaayelenbarrera1145@gmail.com>
2024-05-29 22:14:12 -0300

    Entrega Final

--- curso-react/README.md
+++ curso-react/README.md
@@ -1,8 +1,13 @@
-# React + Vite
+# Coderhouse
+## Curso de React Js con Vite
+- En el curso que realicé en Coderhouse, aprendí a programar con React JS. 
+- Conocí cómo crear interfaces de usuario eficientes, enfocadas en facilitar el desarrollo de aplicaciones de una sola página. 
+- Durante el curso, cubrimos los fundamentos de React, el uso de hooks, la gestión del estado, la configuración de rutas, la comunicación con APIs y las mejores prácticas de desarrollo. 
+- También aprendí a configurar y optimizar proyectos utilizando Vite, una herramienta moderna y rápida de construcción.
 
-This template provides a minimal setup to get React working in Vite with HMR and some ESLint rules.
+### Entrega Final / Ecommerce Tienda Detoxik
 
-Currently, two official plugins are available:
-
-- [@vitejs/plugin-react](https://github.com/vitejs/vite-plugin-react/blob/main/packages/plugin-react/README.md) uses [Babel](https://babeljs.io/) for Fast Refresh
-- [@vitejs/plugin-react-swc](https://github.com/vitejs/vite-plugin-react-swc) uses [SWC](https://swc.rs/) for Fast Refresh
+- Este curso me proporcionó una base sólida para desarrollar una aplicación web moderna y eficiente para un ecommerce llamado Detoxik, donde se ofrece una variedad de productos de lencería. 
+- Los clientes pueden navegar por el menú, buscar los artículos deseados, añadir productos a su carrito y realizar la compra con éxito. 
+- Al completar la compra, obtienen un número de ID para rastrear el estado de su pedido. 
+- Finalmente, los datos de la compra se almacenan en Cloud Firestore, asegurando una gestión eficiente y segura de la información.
--- curso-react/src/components/Checkout.jsx
+++ curso-react/src/components/Checkout.jsx
@@ -1,92 +1,107 @@
-import { useContext, useState } from "react";
+import { useContext, useEffect, useState } from "react";
 import { CartContext } from "./context/CartContext";
 import { addDoc, collection, getFirestore } from "firebase/firestore";
 import { Link } from "react-router-dom";
 
 const Checkout = () => {
-    const { cart, getCountProducts, getSumProducts } = useContext(CartContext);
+    const { cart, clear, getCountProducts, getSumProducts } = useContext(CartContext);
     const [nombre, setNombre] = useState("");
     const [email, setEmail] = useState("");
-    const [telephone, setTelephone] = useState("");
+    const [telefono, setTelefono] = useState("");
     const [orderId, setOrderId] = useState("");
 
-    const generarOrden = async () => {
-        const buyer = { nombre, email, telephone }; // Simplificación de la creación del objeto
-        const items = cart.map(item => ({ id: item.id, title: item.nombre, price: item.precio }));
-        const date = new Date().toLocaleString(); // Simplificación para obtener la fecha actual
-        const order = { buyer, items, date, total: getSumProducts() };
-
-        try {
-            // Obtener la instancia de Firestore
-            const db = getFirestore();
-            // Agregar un nuevo documento a la colección "orders"
-            const docRef = await addDoc(collection(db, "orders"), order);
-            // Establecer el orderId con el ID del documento recién creado
-            setOrderId(docRef.id);
-        } catch (error) {
-            console.error("Error al generar la orden:", error);
+    const generarOrden = () => {
+        if (nombre === "") {
+            return false;
+        } else if (email === "") {
+            return false;
+        } else if (telefono === "") {
+            return false;
         }
-    };
 
-    if (getCountProducts() === 0) {
+        const buyer = { nombre: nombre, email: email, telephone: telefono };
+        const items = cart.map(item => ({ id: item.id, title: item.nombre, price: item.precio }));
+        const date = new Date();
+        const currentDate = `${date.getDate()}-${date.getMonth() + 1}-${date.getFullYear()} ${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}`;
+        const order = { buyer: buyer, items: items, date: currentDate, total: getSumProducts() };
+
+        // Agrego un nuevo Documento a la Colección Orders
+        const db = getFirestore();
+        const ordersCollection = collection(db, "orders");
+        addDoc(ordersCollection, order).then(data => {
+            setOrderId(data.id);
+            setNombre("");
+            setEmail("");
+            setTelefono("");
+            clear();
+        });
+    }
+
+    if (getCountProducts() === 0 && !orderId) {
         return (
             <div className="container my-5">
                 <div className="row">
                     <div className="col text-center">
-                        <h3>No se encontraron productos en el carrito.</h3>
-                        <Link to={"/"} className="btn text-white bg-dark rounded-0 my-5">Volver a la página principal</Link>
+                        <h3>¡El carrito se encuentra vacio!</h3>
+                        <Link to={"/"} className="btn text-white bg-dark rounded-0 my-5">Volver a la Página Principal</Link>
                     </div>
                 </div>
             </div>
-        );
+        )
     }
 
     return (
         <div className="container my-5">
-            <div className="row">
-                <div className="col">
-                    <form>
-                        <div className="mb-3">
-                            <label htmlFor="nombre" className="form-label">Nombre</label>
-                            <input id="nombre" type="text" className="form-control" value={nombre} onChange={(e) => setNombre(e.target.value)} />
-                        </div>
-                        <div className="mb-3">
-                            <label htmlFor="email" className="form-label">Email</label>
-                            <input id="email" type="text" className="form-control" value={email} onChange={(e) => setEmail(e.target.value)} />
-                        </div>
-                        <div className="mb-3">
-                            <label htmlFor="telephone" className="form-label">Teléfono</label>
-                            <input id="telephone" type="text" className="form-control" value={telephone} onChange={(e) => setTelephone(e.target.value)} />
-                        </div>
-                        <button type="button" className="btn text-white bg-dark" onClick={generarOrden}>Generar Orden</button>
-                    </form>
-                </div>
-                <div className="col">
-                    <table className="table">
-                        <tbody>
-                            {cart.map(item => (
-                                <tr key={item.id}>
-                                    <td><img src={item.imagen} alt={item.nombre} width={80} /></td>
-                                    <td>{item.nombre}</td>
-                                    <td>x{item.quantity}</td>
-                                    <td className="text-end">${item.precio}</td>
+            {!orderId ?
+                <div className="row">
+                    <div className="col">
+                        <form>
+                            <div className="mb-3">
+                                <label className="form-label">Nombre</label>
+                                <input type="text" className="form-control" onInput={(e) => { setNombre(e.target.value) }} />
+                            </div>
+                            <div className="mb-3">
+                                <label className="form-label">Email</label>
+                                <input type="text" className="form-control" onInput={(e) => { setEmail(e.target.value) }} />
+                            </div>
+                            <div className="mb-3">
+                                <label className="form-label">Teléfono</label>
+                                <input type="text" className="form-control" onInput={(e) => { setTelefono(e.target.value) }} />
+                            </div>
+
+                            <button type="button" className="btn text-white bg-black" onClick={generarOrden}>Generar Orden</button>
+                        </form>
+                    </div>
+                    <div className="col">
+                        <table className="table">
+                            <tbody>
+                                {cart.map(item => (
+                                    <tr key={item.id}>
+                                        <td><img src={item.imagen} alt={item.nombre} width={80} /></td>
+                                        <td>{item.nombre}</td>
+                                        <td>x{item.quantity}</td>
+                                        <td className="text-end">${item.precio}</td>
+                                    </tr>
+                                ))}
+                                <tr>
+                                    <td colSpan={3}><b>Total</b></td>
+                                    <td className="text-end"><b>${getSumProducts()}</b></td>
                                 </tr>
-                            ))}
-                            <tr>
-                                <td colSpan={3}><b>Total</b></td>
-                                <td className="text-end"><b>${getSumProducts()}</b></td>
-                            </tr>
-                        </tbody>
-                    </table>
-                </div>
-            </div>
+                            </tbody>
+                        </table>
+                    </div>
+                </div> : ""}
             <div className="row my-5">
                 <div className="col text-center">
-                    {orderId && <div className="alert alert-light" role="alert">¡Felicidades! Tu ID de compra es: <b>{orderId}</b></div>}
+                    {orderId ?
+                        <div>
+                            <div className="alert alert-light" role="alert">¡Gracias por tu compra! Tu número de seguimiento es: <b>{orderId}</b></div>
+                            <Link to={"/"} className="btn text-white bg-dark rounded-0 my-3">Volver a la Página Principal</Link>
+                        </div> : ""}
                 </div>
             </div>
         </div>
-    );
-};
+    )
+}
 
 export default Checkout;
